681345e24005eb9dec2e80a01b4138c08a331413
GabrielSantanaDaSilva <topsstdeq@gmail.com>
2024-07-22 10:33:08 -0300

    Config

---
+++ src/main/java/com/generation/farmacia/ProjetoFinalBloco2Application.java
@@ -0,0 +1,13 @@
+package com.generation.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalBloco2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalBloco2Application.class, args);
+	}
+
+}
